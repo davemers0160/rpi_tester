613725b34e6c607e32a88186f2699210cad9b034
david emerson <davemers0160@gmail.com>
2025-06-28 14:02:36 -0400

    Update gpio_test_main.cpp

--- libgpio_multiline_test/src/gpio_test_main.cpp
+++ libgpio_multiline_test/src/gpio_test_main.cpp
@@ -280,7 +280,7 @@ int main(int argc, char** argv)
         // main loop
         while (is_running)
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(5000));
             
             std::cout << "Current Switch Position: " << current_switch_setting << std::endl;
             
